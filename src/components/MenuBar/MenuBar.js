dd7f6c7f2f58e3c5b9d52cca1bfbcf12819277a6
Pawel Durczak <kissonion24@gmail.com>
2022-04-26 23:06:29 +0200

    Added: Burger Menu functionality

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,24 +1,58 @@
-import React from "react";
-import { Container, MenuItem, SocialMediaLinks, MenuItems } from "./Style";
+import React, { useState } from "react";
+import {
+  Container,
+  MenuItem,
+  SocialMediaLinks,
+  MenuItems,
+  MenuBarOutContainer,
+  BurgerMenu,
+  CloseMenuButton,
+} from "./Style";
 import { ReactComponent as FacebookIcon } from "../../assets/facebook-icon.svg";
 import { ReactComponent as InstagramIcon } from "../../assets/instagram-icon.svg";
+import { ReactComponent as MenuIcon } from "../../assets/menu-icon.svg";
+import { ReactComponent as XIcon } from "../../assets/x-icon.svg";
 
 function MenuBar(props) {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const toggleMenu = () => {
+    if (menuOpened) {
+      // Close menu
+      document.getElementById("menu").style.left = "-100vw";
+      setMenuOpened((previous) => !previous);
+    } else {
+      // Open menu
+      document.getElementById("menu").style.left = "0";
+      setMenuOpened((previous) => !previous);
+    }
+  };
+
   return (
-    <Container>
-      <MenuItems>
-        <MenuItem>Zajęcia</MenuItem>
-        <MenuItem>Zespół</MenuItem>
-        <MenuItem>Grafik</MenuItem>
-        <MenuItem>Cennik</MenuItem>
-        <MenuItem>Galeria</MenuItem>
-        <MenuItem>Kontakt</MenuItem>
-      </MenuItems>
-      <SocialMediaLinks>
-        <FacebookIcon></FacebookIcon>
-        <InstagramIcon></InstagramIcon>
-      </SocialMediaLinks>
-    </Container>
+    <>
+      <BurgerMenu onClick={toggleMenu}>
+        <MenuIcon></MenuIcon>
+      </BurgerMenu>
+      <MenuBarOutContainer id="menu">
+        <Container>
+          <CloseMenuButton onClick={toggleMenu}>
+            <XIcon></XIcon>
+          </CloseMenuButton>
+          <MenuItems>
+            <MenuItem>Zajęcia</MenuItem>
+            <MenuItem>Zespół</MenuItem>
+            <MenuItem>Grafik</MenuItem>
+            <MenuItem>Cennik</MenuItem>
+            <MenuItem>Galeria</MenuItem>
+            <MenuItem>Kontakt</MenuItem>
+          </MenuItems>
+          <SocialMediaLinks>
+            <FacebookIcon></FacebookIcon>
+            <InstagramIcon></InstagramIcon>
+          </SocialMediaLinks>
+        </Container>
+      </MenuBarOutContainer>
+    </>
   );
 }
 
--- src/components/MenuBar/Style.js
+++ src/components/MenuBar/Style.js
@@ -1,16 +1,72 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  position: fixed;
-  width: 100%;
-  height: fit-content;
-  border: 1px solid black;
-  padding: 1rem 1rem 0.5rem 1rem;
+const breakingPointPhone = "500px";
 
+export const BurgerMenu = styled.div`
+  position: fixed;
+  top: 0.8rem;
+  left: 0.8rem;
+  aspect-ratio: 1;
+  display: none;
+  z-index: 99;
+  & * {
+    width: 2rem;
+    aspect-ratio: 1;
+    display: grid;
+    place-items: center;
+  }
+  @media screen and (max-width: ${breakingPointPhone}) {
+    display: block;
+  }
+`;
+
+export const CloseMenuButton = styled.div`
+  position: absolute;
+  top: 0.8rem;
+  right: 0.8rem;
+  aspect-ratio: 1;
+  display: none;
+  z-index: 100;
+  & * {
+    width: 1.5rem;
+    aspect-ratio: 1;
+    display: grid;
+    place-items: center;
+  }
+  @media screen and (max-width: ${breakingPointPhone}) {
+    display: block;
+  }
+`;
+
+export const MenuBarOutContainer = styled.div`
+  position: fixed;
+  z-index: 100;
+  width: 100%;
+  display: grid;
+  place-items: center;
+  transition: all 250ms;
+  @media screen and (max-width: ${breakingPointPhone}) {
+    width: 65%;
+    height: 100%;
+    left: -100vw;
+  }
+`;
+
+export const Container = styled.div`
+  width: clamp(500px, 80%, 1000px);
+  height: fit-content;
+
+  padding: 1rem 1rem 0.5rem 1rem;
   display: flex;
   flex-direction: row;
   align-items: center;
   flex-wrap: nowrap;
+  @media screen and (max-width: ${breakingPointPhone}) {
+    flex-direction: column;
+    padding: 0;
+    width: fit-content;
+    height: 90%;
+  }
 `;
 
 export const MenuItem = styled.div`
@@ -20,11 +76,20 @@ export const MenuItem = styled.div`
   padding: 0.2rem;
   display: grid;
   place-items: center;
+  @media screen and (max-width: ${breakingPointPhone}) {
+    margin: 2rem auto;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
 `;
 
 export const MenuItems = styled.div`
   display: flex;
   margin: 0 auto 0 0;
+  @media screen and (max-width: ${breakingPointPhone}) {
+    flex-direction: column;
+    margin: auto;
+  }
 `;
 
 export const SocialMediaLinks = styled.div`
@@ -36,5 +101,8 @@ export const SocialMediaLinks = styled.div`
     margin: 0 0.5rem;
     display: grid;
     place-items: center;
+  }
+  @media screen and (max-width: ${breakingPointPhone}) {
+    margin: auto;
   }
 `;
