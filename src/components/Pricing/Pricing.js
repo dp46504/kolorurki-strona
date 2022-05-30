63281e6747a9a265c3830936f86d7fc7650dd196
Pawel Durczak <kissonion24@gmail.com>
2022-05-30 19:49:44 +0200

    Pricing changed to bookmark view WIP

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Divider,
@@ -11,186 +11,167 @@ import {
 } from "./Style";
 
 function Pricing(props) {
+  const [bookmark, setBookmark] = useState(5);
+  const menuBookmarks = [
+    <>
+      <Title>
+        <div>KARNET</div>
+        <div>POLE/ EXOTIC/ AERIAL</div>
+      </Title>
+
+      <Line>
+        <LineLeft>1 WEJŚCIE</LineLeft>
+        <LineRight>&nbsp;- 50 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>4 WEJŚCIA</LineLeft>
+        <LineRight>&nbsp;- 150 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>8 WEJŚĆ</LineLeft>
+        <LineRight>&nbsp;- 260 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>12 WEJŚĆ</LineLeft>
+        <LineRight>&nbsp;- 370 zł</LineRight>
+      </Line>
+    </>,
+    <>
+      <Title>
+        <div>KARNET</div>
+        <div>ROZCIĄGANIE/WZMACNIANIE/JOGA</div>
+      </Title>
+      <SubTitle>(bez aktywnego karnetu pole/exotic/aerial)</SubTitle>
+
+      <Line>
+        <LineLeft>1 WEJŚCIE</LineLeft>
+        <LineRight>&nbsp;- 35 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>4 WEJŚCIA</LineLeft>
+        <LineRight>&nbsp;- 120 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>8 WEJŚĆ</LineLeft>
+        <LineRight>&nbsp;- 200 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>12 WEJŚĆ</LineLeft>
+        <LineRight>&nbsp;- 280 zł</LineRight>
+      </Line>
+    </>,
+    <>
+      <Title>
+        <div>TRENINGI INDYWIDUALNE</div>
+      </Title>
+
+      <TitleWDivider>1 OSOBA:</TitleWDivider>
+      <Line>
+        <LineLeft>1 WEJŚCIE</LineLeft>
+        <LineRight>&nbsp;- 120 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>4 WEJŚCIA</LineLeft>
+        <LineRight>&nbsp;- 400 zł</LineRight>
+      </Line>
+
+      <TitleWDivider>2 OSOBY:</TitleWDivider>
+      <Line>
+        <LineLeft>1 WEJŚCIE</LineLeft>
+        <LineRight>&nbsp;- 180 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>4 WEJŚCIA</LineLeft>
+        <LineRight>&nbsp;- 650 zł</LineRight>
+      </Line>
+    </>,
+    <>
+      <Title>OPEN</Title>
+      <SubTitle>(godzina na rurce bez nadzoru trenera)</SubTitle>
+
+      <TitleWDivider>z aktywnym karnetem pole/exotic/aerial</TitleWDivider>
+
+      <Line>
+        <LineLeft>1 WEJŚCIE</LineLeft>
+        <LineRight>&nbsp;- 15 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>4 WEJŚCIA</LineLeft>
+        <LineRight>&nbsp;- 50 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>NIELIMITOWANY</LineLeft>
+        <LineRight>&nbsp;- 120 zł</LineRight>
+      </Line>
+
+      <TitleWDivider>bez aktywnego karnetu pole/exotic/aerial</TitleWDivider>
+      <Line>
+        <LineLeft>1 WEJŚCIE</LineLeft>
+        <LineRight>&nbsp;- 25 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>4 WEJŚCIA</LineLeft>
+        <LineRight>&nbsp;- 80 zł</LineRight>
+      </Line>
+      <Line>
+        <LineLeft>NIELIMITOWANY</LineLeft>
+        <LineRight>&nbsp;- 200 zł</LineRight>
+      </Line>
+    </>,
+    <>
+      <Title>AERIAL HOOP</Title>
+      <SubTitle>
+        zespół KR wprowadził dwie możliwości uczęszczania na zajęcia Aerial
+        Hoop!
+      </SubTitle>
+
+      <TitleWDivider>WARIANT 1</TitleWDivider>
+      <SubTitle>
+        Wykupienie standardowego karnetu miesięcznego, który pozwala Ci
+        uczęszczać na zajęcia na kole jeden raz w tygodniu, we wcześniej
+        ustalonych dniach i godzinach. Jak wiecie z cennika - koszt takiego
+        karnetu to{" "}
+        <span style={{ fontWeight: "bold", fontFamily: "Playfair Display" }}>
+          150 zł
+        </span>{" "}
+        i charakteryzuje go fakt, że
+        <div style={{ fontWeight: "bold", fontFamily: "Playfair Display" }}>
+          MASZ KOŁO WYŁĄCZNIE DLA SIEBIE
+        </div>
+      </SubTitle>
+      <TitleWDivider>WARIANT 1</TitleWDivider>
+      <SubTitle>
+        Opcja druga jest w pewnym sensie opcją rabatową! Zapłać za zajęcia{" "}
+        <span style={{ fontWeight: "bold", fontFamily: "Playfair Display" }}>
+          120 zł
+        </span>{" "}
+        miesięcznie przy tych samych zasadach uczęszczania co powyżej. Musisz
+        jednak liczyć się z tym, że w każdej chwili może pojawić się Twój
+        "Towarzysz Od Koła", z którym będziesz je współdzielił/a. Jest to
+        świetna informacja dla osób, które dopiero zaczynają swoją podniebną
+        zabawę - wierzcie... wysiłek jest niemały, więc chwila na odpoczynek
+        dzięki pracy w duecie czasami wskazana :D
+      </SubTitle>
+    </>,
+    <>
+      <Title>
+        <div style={{ fontWeight: "bold" }}>KARNET VIP W PRZEDSPRZEDAŻY!</div>
+        <div style={{ fontWeight: "bold" }}>490zł</div>
+      </Title>
+      <SubTitle>
+        Karnet upoważnia do wstępu na wszystkie prowadzone zajęcia grupowe, bez
+        limitu wejść. Karnet ważny jest przez miesiąc. Nie ogranicza Cię nic,
+        poza ilością godzin w dobie i Twoim zapałem.
+      </SubTitle>
+    </>,
+  ];
+
   return (
     <>
       <Container>
-        <Title>
-          <div>KARNET</div>
-          <div>POLE/ EXOTIC/ AERIAL</div>
-        </Title>
-
-        <Line>
-          <LineLeft>1 WEJŚCIE</LineLeft>
-          <LineRight>&nbsp;- 50 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>4 WEJŚCIA</LineLeft>
-          <LineRight>&nbsp;- 150 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>8 WEJŚĆ</LineLeft>
-          <LineRight>&nbsp;- 260 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>12 WEJŚĆ</LineLeft>
-          <LineRight>&nbsp;- 370 zł</LineRight>
-        </Line>
-
+        <Divider side={1}></Divider>
+        {menuBookmarks[bookmark]}
         <Divider></Divider>
-
-        <Title>
-          <div>KARNET</div>
-          <div>ROZCIĄGANIE/WZMACNIANIE/JOGA</div>
-        </Title>
-        <SubTitle>(z aktywnym karnetem pole/exotic/aerial)</SubTitle>
-
-        <Line>
-          <LineLeft>1 WEJŚCIE</LineLeft>
-          <LineRight>&nbsp;- 25 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>4 WEJŚCIA</LineLeft>
-          <LineRight>&nbsp;- 80 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>8 WEJŚĆ</LineLeft>
-          <LineRight>&nbsp;- 140 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>12 WEJŚĆ</LineLeft>
-          <LineRight>&nbsp;- 200 zł</LineRight>
-        </Line>
-
-        <Divider></Divider>
-        <Title>
-          <div>KARNET</div>
-          <div>ROZCIĄGANIE/WZMACNIANIE/JOGA</div>
-        </Title>
-        <SubTitle>(bez aktywnego karnetu pole/exotic/aerial)</SubTitle>
-
-        <Line>
-          <LineLeft>1 WEJŚCIE</LineLeft>
-          <LineRight>&nbsp;- 35 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>4 WEJŚCIA</LineLeft>
-          <LineRight>&nbsp;- 120 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>8 WEJŚĆ</LineLeft>
-          <LineRight>&nbsp;- 200 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>12 WEJŚĆ</LineLeft>
-          <LineRight>&nbsp;- 280 zł</LineRight>
-        </Line>
-
-        <Divider></Divider>
-
-        <Title>
-          <div>TRENINGI INDYWIDUALNE</div>
-        </Title>
-
-        <TitleWDivider>1 OSOBA:</TitleWDivider>
-        <Line>
-          <LineLeft>1 WEJŚCIE</LineLeft>
-          <LineRight>&nbsp;- 120 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>4 WEJŚCIA</LineLeft>
-          <LineRight>&nbsp;- 400 zł</LineRight>
-        </Line>
-
-        <TitleWDivider>2 OSOBY:</TitleWDivider>
-        <Line>
-          <LineLeft>1 WEJŚCIE</LineLeft>
-          <LineRight>&nbsp;- 180 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>4 WEJŚCIA</LineLeft>
-          <LineRight>&nbsp;- 650 zł</LineRight>
-        </Line>
-
-        <Divider></Divider>
-
-        <Title>OPEN</Title>
-        <SubTitle>(godzina na rurce bez nadzoru trenera)</SubTitle>
-
-        <TitleWDivider>z aktywnym karnetem pole/exotic/aerial</TitleWDivider>
-
-        <Line>
-          <LineLeft>1 WEJŚCIE</LineLeft>
-          <LineRight>&nbsp;- 15 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>4 WEJŚCIA</LineLeft>
-          <LineRight>&nbsp;- 50 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>NIELIMITOWANY</LineLeft>
-          <LineRight>&nbsp;- 120 zł</LineRight>
-        </Line>
-
-        <TitleWDivider>bez aktywnego karnetu pole/exotic/aerial</TitleWDivider>
-        <Line>
-          <LineLeft>1 WEJŚCIE</LineLeft>
-          <LineRight>&nbsp;- 25 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>4 WEJŚCIA</LineLeft>
-          <LineRight>&nbsp;- 80 zł</LineRight>
-        </Line>
-        <Line>
-          <LineLeft>NIELIMITOWANY</LineLeft>
-          <LineRight>&nbsp;- 200 zł</LineRight>
-        </Line>
-
-        <Divider></Divider>
-
-        <Title>AERIAL HOOP</Title>
-        <SubTitle>
-          zespół KR wprowadził dwie możliwości uczęszczania na zajęcia Aerial
-          Hoop!
-        </SubTitle>
-
-        <TitleWDivider>WARIANT 1</TitleWDivider>
-        <SubTitle>
-          Wykupienie standardowego karnetu miesięcznego, który pozwala Ci
-          uczęszczać na zajęcia na kole jeden raz w tygodniu, we wcześniej
-          ustalonych dniach i godzinach. Jak wiecie z cennika - koszt takiego
-          karnetu to{" "}
-          <span style={{ fontWeight: "bold", fontFamily: "Playfair Display" }}>
-            150 zł
-          </span>{" "}
-          i charakteryzuje go fakt, że
-          <div style={{ fontWeight: "bold", fontFamily: "Playfair Display" }}>
-            MASZ KOŁO WYŁĄCZNIE DLA SIEBIE
-          </div>
-        </SubTitle>
-        <TitleWDivider>WARIANT 1</TitleWDivider>
-        <SubTitle>
-          Opcja druga jest w pewnym sensie opcją rabatową! Zapłać za zajęcia{" "}
-          <span style={{ fontWeight: "bold", fontFamily: "Playfair Display" }}>
-            120 zł
-          </span>{" "}
-          miesięcznie przy tych samych zasadach uczęszczania co powyżej. Musisz
-          jednak liczyć się z tym, że w każdej chwili może pojawić się Twój
-          "Towarzysz Od Koła", z którym będziesz je współdzielił/a. Jest to
-          świetna informacja dla osób, które dopiero zaczynają swoją podniebną
-          zabawę - wierzcie... wysiłek jest niemały, więc chwila na odpoczynek
-          dzięki pracy w duecie czasami wskazana :D
-        </SubTitle>
-
-        <Divider></Divider>
-        <Title>
-          <div style={{ fontWeight: "bold" }}>KARNET VIP W PRZEDSPRZEDAŻY!</div>
-          <div style={{ fontWeight: "bold" }}>490zł</div>
-        </Title>
-        <SubTitle>
-          Karnet upoważnia do wstępu na wszystkie prowadzone zajęcia grupowe,
-          bez limitu wejść. Karnet ważny jest przez miesiąc. Nie ogranicza Cię
-          nic, poza ilością godzin w dobie i Twoim zapałem.
-        </SubTitle>
       </Container>
     </>
   );
